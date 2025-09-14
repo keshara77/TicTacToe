466ba310f5e582748140ffad423d49f22bd13994
keshara77 <it25102791@my.sliit.lk>
2025-09-14 11:31:47 -0700

    First file
    first file

---
+++ game.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int n;
+
+void initialize(){
+	printf("This is the Tic Tac Toe Game\n");
+	printf("Enter board size N(3 <= N <= 10) - \n");
+	sacnf("%d",&n);
+	};
+
+void Game_Board(){
+	
+	
